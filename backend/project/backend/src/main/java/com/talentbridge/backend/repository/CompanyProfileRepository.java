e7988b2f7e6038f5915760a41affbdf9f51a918d
Preet Brahmbhatt <138224316+preet1694@users.noreply.github.com>
2025-03-20 00:25:46 +0530

    Changing directory structure

--- backend/project/backend/src/main/java/com/talentbridge/backend/repository/CompanyProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.talentbridge.backend.repository;
-
-import com.talentbridge.backend.model.CompanyProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyProfileRepository extends JpaRepository<CompanyProfile, Long> {
-}
--- backend/project/backend/src/main/java/com/talentbridge/backend/repository/UserProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.talentbridge.backend.repository;
-
-import com.talentbridge.backend.model.UserProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
-}
